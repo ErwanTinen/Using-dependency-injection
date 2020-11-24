2f2e91f4fb976d8129e9785c373aae0100f2af3d
erwan.tinentouolac@etu.emse.fr <erwan.tinentouolac@etu.emse.fr>
2020-11-24 17:28:33 +0100

    lab 4 finished

---
+++ src/main/java/com/emse/spring/faircorp/model/Room.java
@@ -0,0 +1,95 @@
+package com.emse.spring.faircorp.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Room {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable=false)
+    private Integer floor;
+
+    @Column(nullable=false)
+    private String name;
+
+    private Double currentTemperature;
+
+    private Double targetTemperature;
+
+    @OneToMany(mappedBy = "room")
+    private Set<Heater> heaters;
+
+    @OneToMany(mappedBy = "room")
+    private Set<Window> windows;
+
+    public Room() {
+    }
+
+    public Room(Long id, String name,Integer floor) {
+        this.id = id;
+        this.floor = floor;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public Double getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public Double getTargetTemperature() {
+        return targetTemperature;
+    }
+
+
+    public Set<Heater> getHeaters() {
+        return heaters;
+    }
+
+    public Set<Window> getWindows() {
+        return windows;
+    }
+
+    public void setFloor(Integer floor) {
+        this.floor = floor;
+    }
+
+    public void setCurrentTemperature(Double currentTemperature) {
+        this.currentTemperature = currentTemperature;
+    }
+
+    public void setTargetTemperature(Double targetTemperature) {
+        this.targetTemperature = targetTemperature;
+    }
+
+    public void setHeaters(Set<Heater> heaters) {
+        this.heaters = heaters;
+    }
+
+    public void setWindows(Set<Window> windows) {
+        this.windows = windows;
+    }
+
+}
